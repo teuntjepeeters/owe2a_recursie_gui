b4f0504956e4584d75552e2f03485538a33691af
lussj <teuntje.peeters@han.nl>
2021-12-21 13:20:33 +0100

    GUI checkbuttons naar git

--- cd/gui.py
+++ cd/gui.py
@@ -15,14 +15,30 @@ class GUI:
         self.bottom_frame.pack()
 
         # Maak labels aan
-        self.label1 = tkinter.Label(self.bottom_frame,               # 1. Waar komt dit label te staan?,
+        self.label1 = tkinter.Label(self.top_frame,               # 1. Waar komt dit label te staan?,
                                     text="Bijna kerstvakantie!")    # 2. Welke tekst komt in het label te staan?
         self.label1.pack()
         self.label2 = tkinter.Label(self.top_frame,
                                     text="Bijna 2022!")
         self.label2.pack()
+
+        # Maak knoppen aan
+        self.knopje = tkinter.Button(self.bottom_frame, # Waar komt de button te staan?
+                                     text="Klik hier!", # Tekst die weergegeven wordt op de button
+                                     command=self.action)  # Functie die wordt aangeroepen als er op de knop wordt gedrukt
+        self.knopje.pack()
+        self.quit_button = tkinter.Button(self.bottom_frame,
+                                          text="QUIT",
+                                          command=self.main_window.destroy)
+        self.quit_button.pack()
+
         # Roep de main window aan
         tkinter.mainloop()
+
+    def action(self):
+        tkinter.messagebox.showinfo("Response",
+                                    "Bedankt voor het klikken!")
+        print("Hello world!")
 
 def main():
     gui = GUI()
